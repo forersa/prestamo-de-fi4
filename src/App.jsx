75b08e5cf3f3c1495f91a5a7757cfb9507e6927e
forersa <forersa@yahoo.com>
2024-04-11 06:24:50 +0200

    Configuracion header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import Header from "./components/Header"
+
 function App() {
-  
-   return (
-    <h1 className="bg-blue-500 text-3xl font-bold underline">
-      Hello world!
-    </h1>
+     return (
+    <>
+      <Header />
+      <h1 className="bg-blue-500 text-3xl font-bold underline">
+      Hello world!</h1>
+    </>
+    
   )
 }
 export default App
