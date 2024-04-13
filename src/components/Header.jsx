f0ad646d514031935d8b6c724db9935c11c890c3
forersa <forersa@yahoo.com>
2024-04-13 06:34:44 +0200

    Añadir componente header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,13 @@
 export default function Header(){
-    return <header>Banco DeFi</header>
+    return (
+      <header className=" border-b shadow-8xl bg-red-300 py-2 px-3 flex justify-between items-center">
+        {/* Logo para version mobile */}
+        <img src="/LogoBanco.jpeg" alt="BancoDeFi-mobile-logo" className="sm:hidden" width={40}/>
+        {/* Logo para version desktop*/}
+        <img src="/LogoLargo.JPG" alt="BancoDeFi-desktop-logo" className="hidden sm:block" width={150} />
+        <button className="bg-blue-300 rounded-2xl px-5 text-justify h-fit">Connect Wallet</button>
+    
+    
+    </header>
+    )
 }
