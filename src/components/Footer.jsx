23cdf9dfdee0da89579b74654095225bbbedad87
forersa <forersa@yahoo.com>
2024-04-13 06:36:38 +0200

    Añadir componente Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,4 @@
+export default function Footer(){
+    return <footer className="border-t py-6 px-5 flex justify-center bg-zinc-400 shadow-xl">
+        <p>Derechos de Autor &copy; {new Date().getFullYear()} Sander Smith Forero</p></footer>
+}
